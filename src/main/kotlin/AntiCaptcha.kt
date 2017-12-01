d98496d651cf9ffb74054d1640c34896f9c9dd6a
norenma <markus.a.noren@gmail.com>
2017-12-01 16:06:31 +0100

    first day of christmas

---
+++ src/main/kotlin/AntiCaptcha.kt
@@ -0,0 +1,37 @@
+/**
+ *
+ *
+ * @author markusanderssonnoren
+ * @since 2017-12-01.
+ */
+package Day1
+
+import java.lang.Integer.parseInt
+
+/**
+ *
+ *
+ * @author markusanderssonnoren
+ * @since 2017-12-01.
+ */
+
+fun formatString(s: String) : List<Int> {
+    return s.toCharArray().map { s -> parseInt(s.toString()) };
+}
+
+fun doAntiCaptcha(s: List<Int> ) : Int {
+    return s.reduceIndexed {i, sum, digit ->
+        val next = if (i != s.size - 1) s[i + 1] else s[1]
+        if(digit == next) sum + digit;
+        else sum;
+    }
+}
+
+fun doAntiCaptcha2(s: List<Int> ) : Int {
+    return s.foldIndexed (0, {i, sum, digit ->
+        val next = s[(i + s.size / 2) % s.size ]
+        if(digit == next) sum + digit;
+        else sum;
+    })
+}
+
